ca5610a73f977b9858d4b082aadd31d2d895f055
Daren Ldl <darrenldldev@gmail.com>
2022-11-18 01:18:16 +1100

    Updating benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -4,8 +4,13 @@ import os
 from bench_utils import *
 
 parser = argparse.ArgumentParser(description='Benchmark.')
-parser.add_argument('--dir', help='directory to use')
+# parser.add_argument('--dir', help='directory to use')
 parser.add_argument('--pat', help='pattern')
+parser.add_argument('--tgonly', action="store_true")
+parser.add_argument('--tmonly', action="store_true")
+parser.add_argument('--core', help='core count', default=4)
+parser.add_argument('--maxmemory', help='maximum memory', default="50G")
+parser.add_argument('--timeout', help='timeout for Tamarin command', default="60m")
 
 args = parser.parse_args()
 
@@ -14,3 +19,22 @@ if args.pat is None:
 else:
     pattern = args.pat + ".tg"
 
+config = {}
+
+config["core"] = args.core
+config["maxmemory"] = args.maxmemory
+config["timeout"] = args.timeout
+
+if args.tgonly and args.tmonly:
+    print("--tgonly conflicts with --tmonly")
+    exit(1)
+
+if args.tgonly:
+    config["exts"] = [ ".tg.spthy" ]
+elif args.tmonly:
+    config["exts"] = [ ".spthy" ]
+else:
+    config["exts"] = [ ".spthy", ".tg.spthy" ]
+
+for ext in config["exts"]:
+    print(ext)
