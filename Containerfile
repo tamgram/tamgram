5b70a0a6357e83f2a4b2c14772453604993b92a0
Darren Li <darrenldldev@gmail.com>
2023-08-09 03:54:43 +1000

    Updated container related files

--- Containerfile
+++ Containerfile
@@ -1,5 +1,4 @@
 FROM docker.io/ocaml/opam:alpine-ocaml-4.14
-USER root
 RUN opam init --disable-sandboxing
 RUN opam install dune containers fmt
 RUN opam install menhir
--- start-container.sh
+++ start-container.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-podman run -it -v ~/tamgram:/root/tamgram --rm localhost/tamgram
+podman run -it \
+  -v ~/tamgram:/home/opam/tamgram \
+  --userns keep-id:uid=$(id -u),gid=$(id -g) \
+  --rm \
+  localhost/tamgram
