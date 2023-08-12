5be9623c029695d08f3b4e567300732cc3b5a753
Darren Li <darrenldldev@gmail.com>
2023-08-12 16:59:27 +1000

    Fixed start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 podman run -it \
   -v ~/tamgram:/home/opam/tamgram \
-  --userns keep-id:uid=$(id -u),gid=$(id -g) \
+  --userns keep-id:uid=1000,gid=1000 \
   --rm \
   localhost/tamgram
