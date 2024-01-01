c57ae47f7acce655a768598958ffee4d86d0bdae
Darren Li <darrenldldev@gmail.com>
2024-01-01 18:11:05 +1100

    Adding additional IR to hybrid translation

--- src/tr_frame_minimal_hybrid0.ml
+++ src/tr_frame_minimal_hybrid0.ml
@@ -14,58 +14,30 @@ let rule_is_empty (spec : Spec.t) (g : Tg_graph.t) (k : int) =
   cell_pat_matches = []
   && ru.l = [] && ru.a = [] && ru.r = []
 
-let exit_fact_to_empty_rule (spec : Spec.t) g (k : int) ~empty_rule : Tg_ast.term =
-  let ctx_r =
-    Int_map.find empty_rule spec.cells_to_carry_over_before
-  in
-  let ru = Graph.find k g in
-  let cell_subs = cell_subs_of_rule spec g k in
-  let _r, assigns =
-    clean_up_r cell_subs Tg_ast.(ru.r)
-  in
-  let frame =
-    String_tagged_set.to_seq ctx_r
-    |> Seq.map (fun c ->
-        List.assoc (Loc.content c) (assigns @ cell_subs)
-      )
-    |> List.of_seq
-  in
-  T_app { path = Path.of_string "St";
-          name = `Global 0;
-          named_args = [];
-          args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
-                 ; T_value (Loc.untagged (`Str
-                                            (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix empty_rule)))
-                 ; T_tuple (None, frame)
-                 ];
-          anno = None;
-        }
+module State_fact_IR = struct
+  type t = {
+    bias : exit_bias;
+    k : int;
+    frame : Tg_ast.term list;
+  }
 
-let entry_fact_from_empty_rule (spec : Spec.t) g (k : int) ~empty_rule : Tg_ast.term =
-  let ctx_r =
-    Int_map.find empty_rule spec.cells_to_carry_over_before
-  in
-  let cell_subs = cell_subs_of_rule spec g k in
-  let frame =
-    String_tagged_set.to_seq ctx_r
-    |> Seq.map (fun c ->
-        List.assoc (Loc.content c) cell_subs
-      )
-    |> List.of_seq
-  in
-  T_app { path = Path.of_string "St";
-          name = `Global 0;
-          named_args = [];
-          args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
-                 ; T_value (Loc.untagged (`Str
-                                            (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix empty_rule)))
-                 ; T_tuple (None, frame)
-                 ];
-          anno = None;
-        }
+  let to_term (t : t) : Tg_ast.term =
+    T_app {
+      path = (match t.bias with
+          | `Forward -> Path.of_string "StF"
+          | `Backward -> Path.of_string "StB");
+      name = `Global 0;
+      named_args = [];
+      args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
+             ; T_value (Loc.untagged (`Str (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix t.k)))
+             ; T_tuple (None, t.frame)
+             ];
+      anno = None;
+    }
+end
 
 module Forward_biased = struct
-  let entry_fact (spec : Spec.t) g (k : int) : Tg_ast.term =
+  let entry_fact (spec : Spec.t) g (k : int) : State_fact_IR.t =
     let ctx_r =
       Int_map.find k spec.cells_to_carry_over_before
     in
@@ -77,17 +49,13 @@ module Forward_biased = struct
         )
       |> List.of_seq
     in
-    T_app { path = Path.of_string "StF";
-            name = `Global 0;
-            named_args = [];
-            args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
-                   ; T_value (Loc.untagged (`Str (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix k)))
-                   ; T_tuple (None, frame_l)
-                   ];
-            anno = None;
-          }
+    {
+      bias = `Forward;
+      k;
+      frame = frame_l;
+    }
 
-  let exit_fact (spec : Spec.t) (g : Tg_graph.t) (k : int) ~succ : Tg_ast.term =
+  let exit_fact (spec : Spec.t) (g : Tg_graph.t) (k : int) ~succ : State_fact_IR.t =
     let ru = Graph.find k g in
     let ctx_r_of_succ =
       Int_map.find succ spec.cells_to_carry_over_before
@@ -103,19 +71,15 @@ module Forward_biased = struct
         )
       |> List.of_seq
     in
-    T_app { path = Path.of_string "StF";
-            name = `Global 0;
-            named_args = [];
-            args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
-                   ; T_value (Loc.untagged (`Str (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix succ)))
-                   ; T_tuple (None, frame_r)
-                   ];
-            anno = None;
-          }
+    {
+      bias = `Forward;
+      k = succ;
+      frame = frame_r;
+    }
 end
 
 module Backward_biased = struct
-  let entry_fact (spec : Spec.t) g ~pred (k : int) : Tg_ast.term =
+  let entry_fact (spec : Spec.t) g ~pred (k : int) : State_fact_IR.t =
     let ctx_ra_of_pred =
       Int_map.find pred spec.cells_to_carry_over_after
     in
@@ -127,17 +91,13 @@ module Backward_biased = struct
         )
       |> List.of_seq
     in
-    T_app { path = Path.of_string "StB";
-            name = `Global 0;
-            named_args = [];
-            args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
-                   ; T_value (Loc.untagged (`Str (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix pred)))
-                   ; T_tuple (None, frame_l)
-                   ];
-            anno = None;
-          }
+    {
+      bias = `Backward;
+      k = pred;
+      frame = frame_l;
+    }
 
-  let exit_fact (spec : Spec.t) (g : Tg_graph.t) (k : int) : Tg_ast.term =
+  let exit_fact (spec : Spec.t) (g : Tg_graph.t) (k : int) : State_fact_IR.t =
     let ru = Graph.find k g in
     let ctx_ra =
       Int_map.find k spec.cells_to_carry_over_after
@@ -153,15 +113,11 @@ module Backward_biased = struct
         )
       |> List.of_seq
     in
-    T_app { path = Path.of_string "StB";
-            name = `Global 0;
-            named_args = [];
-            args = [ T_var (Path.of_string "pid", `Global 0, Some `Fresh)
-                   ; T_value (Loc.untagged (`Str (Printf.sprintf "%s%d" Params.graph_vertex_label_prefix k)))
-                   ; T_tuple (None, frame_r)
-                   ];
-            anno = None;
-          }
+    {
+      bias = `Backward;
+      k;
+      frame = frame_r;
+    }
 end
 
 let exit_bias (spec : Spec.t) (g : Tg_graph.t) (k : int) : exit_bias =
@@ -175,7 +131,7 @@ let exit_bias (spec : Spec.t) (g : Tg_graph.t) (k : int) : exit_bias =
       `Backward
   )
 
-let compute_possible_exit_facts spec g k : (int option * Tg_ast.term) Seq.t =
+let compute_possible_exit_facts spec g k : (int option * State_fact_IR.t) Seq.t =
   match exit_bias spec g k with
   | `Forward -> (
       Graph.succ_seq k g
@@ -187,19 +143,18 @@ let compute_possible_exit_facts spec g k : (int option * Tg_ast.term) Seq.t =
       Seq.return (None, Backward_biased.exit_fact spec g k)
     )
 
-let compute_possible_entry_facts spec g k : (int option * Tg_ast.term) Seq.t =
+let compute_possible_entry_facts spec g k : (int option * State_fact_IR.t) Seq.t =
   let
     forward_biased_exit_preds,
-    backward_biased_exit_preds,
-    empty_rule_exit_preds
+    backward_biased_exit_preds
     =
-    Int_set.fold (fun x (f, b, e) ->
+    Int_set.fold (fun x (f, b) ->
         match exit_bias spec g x with
-        | `Forward -> (Int_set.add x f, b, e)
-        | `Backward -> (f, Int_set.add x b, e)
+        | `Forward -> (Int_set.add x f, b)
+        | `Backward -> (f, Int_set.add x b)
       )
       (Graph.pred k g)
-      (Int_set.empty, Int_set.empty, Int_set.empty)
+      (Int_set.empty, Int_set.empty)
   in
   [
     (if Int_set.is_empty forward_biased_exit_preds then Seq.empty
@@ -210,11 +165,6 @@ let compute_possible_entry_facts spec g k : (int option * Tg_ast.term) Seq.t =
     (Int_set.to_seq backward_biased_exit_preds
      |> Seq.map (fun pred ->
          (Some pred, Backward_biased.entry_fact spec g k ~pred)
-       )
-    );
-    (Int_set.to_seq empty_rule_exit_preds
-     |> Seq.map (fun empty_rule ->
-         (Some empty_rule, entry_fact_from_empty_rule spec g k ~empty_rule)
        )
     );
   ]
@@ -245,6 +195,11 @@ let pp_rule_id
     k
     pp_link_target succ
 
+type rule_ir = {
+  entry_fact : State_fact_IR.t;
+  exit_fact : State_fact_IR.t;
+}
+
 let start_tr (binding : Tg_ast.proc Binding.t) (spec : Spec.t) : Tg_ast.decl Seq.t =
   let open Tg_ast in
   let g = Name_map.find (Binding.name binding) spec.proc_graphs in
@@ -259,7 +214,7 @@ let start_tr (binding : Tg_ast.proc Binding.t) (spec : Spec.t) : Tg_ast.decl Seq
       in
       compute_possible_exit_facts spec g k
       |> Seq.map (fun (dst, exit_fact) ->
-          let r = exit_fact :: r in
+          let r = State_fact_IR.to_term exit_fact :: r in
           let rule_name =
             Fmt.str "%a___%a%s" pp_name_of_proc binding
               (pp_rule_id
@@ -304,10 +259,10 @@ let rule_tr (binding : Tg_ast.proc Binding.t) (spec : Spec.t) : Tg_ast.decl Seq.
       in
       compute_possible_exit_facts spec g k
       |> Seq.flat_map (fun (dst, exit_fact) ->
-          let r = exit_fact :: r in
+          let r = State_fact_IR.to_term exit_fact :: r in
           compute_possible_entry_facts spec g k
           |> Seq.map (fun (src, entry_fact) ->
-              let l = entry_fact :: l in
+              let l = State_fact_IR.to_term entry_fact :: l in
               let rule_name =
                 Fmt.str "%a___%a%s" pp_name_of_proc binding
                   (pp_rule_id
@@ -355,7 +310,7 @@ let end_tr (binding : Tg_ast.proc Binding.t) (spec : Spec.t) : Tg_ast.decl Seq.t
       in
       compute_possible_entry_facts spec g k
       |> Seq.map (fun (src, entry_fact) ->
-          let l = entry_fact :: l in
+          let l = State_fact_IR.to_term entry_fact :: l in
           let rule_name =
             Fmt.str "%a___%a%s" pp_name_of_proc binding
               (pp_rule_id
